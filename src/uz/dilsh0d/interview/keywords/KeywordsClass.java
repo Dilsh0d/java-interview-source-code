f4a1c95b451df25b2788f0dca15475a0ef3162e0
Dilshod Tadjiev <dilshod.toj@gmail.com>
2021-06-23 12:39:36 +0500

    List of Java keywords

---
+++ src/uz/dilsh0d/interview/keywords/KeywordsClass.java
@@ -0,0 +1,124 @@
+package uz.dilsh0d.interview.keywords;
+
+/**
+ * List of Java keywords
+ * <br/>
+ *
+ * In the Java programming language, a keyword is any one of 52 reserved words[1]
+ * that have a predefined meaning in the language; because of this, programmers cannot use
+ * keywords as names for variables, methods, classes, or as any other identifier.[2]
+ * Of these 52 keywords, 49 are in use, 1 is in preview, and 2 are not in use.
+ * Due to their special functions in the language, most integrated development
+ * environments for Java use syntax highlighting to display keywords
+ * in a different colour for easy identification.
+ *
+ * @author Tadjiev Dilshod
+ */
+public class KeywordsClass {
+
+    /** Data types (8)
+     *
+     *  byte
+     *  short
+     *  int
+     *  long
+     *  float
+     *  double
+     *  boolean
+     *  char
+     */
+
+    /**
+     * Flow control(10)
+     *
+     * if
+     * else
+     * switch
+     * case
+     * default
+     * break
+     * continue
+     * for
+     * while
+     * do
+     *
+     */
+
+    /**
+     * Modifier(11)
+     *
+     * public
+     * protected
+     * private
+     * static
+     * final
+     * abstract
+     * synchronized
+     * native
+     * strictfp
+     * transient
+     * volatile
+     *
+     */
+
+    /**
+     * Exception handling(6)
+     *
+     * try
+     * catch
+     * finally
+     * throw
+     * throws
+     * assert
+     *
+     */
+
+    /**
+     * Class related (7)
+     *
+     * class
+     * package
+     * import
+     * extends
+     * interface
+     * implements
+     * enum  [added in J2SE 5.0]
+     */
+
+    /**
+     * Object related (4)
+     *
+     * new
+     * instanceof
+     * super
+     * this
+     *
+     */
+
+    /**
+     * Method related (2)
+     *
+     * void
+     * return
+     *
+     */
+
+    /**
+     * Reserved literals (3)
+     *
+     * true
+     * false
+     * null
+     *
+     */
+
+    /**
+     * Unused (2)
+     *
+     * const
+     * goto
+     */
+
+    // 53 key word of Java
+
+}
