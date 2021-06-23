91df810047eb936b1739fc74b1722cc27a382be0
Dilshod Tadjiev <dilshod.toj@gmail.com>
2021-06-23 17:32:48 +0500

    Byte code in java.

--- src/uz/dilsh0d/interview/baytecode/WORA.java
+++ src/uz/dilsh0d/interview/baytecode/WORA.java
@@ -8,7 +8,9 @@ package uz.dilsh0d.interview.baytecode;
  * @author Tadjiev Dilshod
  */
 public class WORA {
-
+    /**
+     * Java *.file ->  Java *.class of byte code - > Java Virtual Machine -> machine code
+     */
     public static void main(String[] args) {
         System.out.println("\"Write once, read anywhere\" or" + " \"WORA\"");
     }
