11038a6c6e7316d24e2a3b32a5ca365141469b27
Dilshod Tadjiev <dilshod.toj@gmail.com>
2021-06-23 16:44:02 +0500

    Byte code in java.

---
+++ src/uz/dilsh0d/interview/baytecode/WORA.java
@@ -0,0 +1,15 @@
+package uz.dilsh0d.interview.baytecode;
+
+/**
+ *  Supporting this, Sun Microsystems captioned
+ *  JAVA as "write once, read anywhere" or
+ *  "WORA" in resonance to the bytecode interpretation.
+ *
+ * @author Tadjiev Dilshod
+ */
+public class WORA {
+
+    public static void main(String[] args) {
+        System.out.println("\"Write once, read anywhere\" or" + " \"WORA\"");
+    }
+}
